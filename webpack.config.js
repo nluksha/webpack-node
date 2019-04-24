d6deed868d46e83309f2aa916b6d742323b66b72
Mikita Luksha <nikita.luksha@gmail.com>
2019-04-24 17:56:09 +0300

    Add index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
-  output: { 
+  output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/assets/'
+    filename: 'bundle.js'
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Dev',
+      template: 'app/index.html',
+      filename: 'index.html'
+    })
+  ],
 
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     hot: true,
-    inline: true,
-    port: 9000
+    inline: true
   },
 
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|dist)/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
